23f70a45c55d1f7f579dd5c2b771888e2b427b55
israfil <israfil3019@gmail.com>
2021-05-27 19:14:42 +0300

    add list and to-do item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Todos</h1>
+      <TodoInput />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
